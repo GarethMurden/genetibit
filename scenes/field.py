d45ce5c3be496de10d26865fd76a5c8b8938a86d
Gareth Murden <Gareth.Murden@email.com>
2025-08-25 13:52:23 +0100

    remove redundant items

--- scenes/field.py
+++
@@ -1,9 +0,0 @@
-
-import os
-from PIL import Image
-
-dirname, _ = os.path.split(os.path.abspath(__file__))
-THIS_DIRECTORY = f'{dirname}{os.sep}'
-
-def draw(creatures, level=0):
-	background = Image.open(f'{THIS_DIRECTORY}assets{os.sep}background{os.sep}field_{level}.png')
